5695261513d31d3041c7632925810e860f4f435b
minigonche <minigonche@minigonche-macbook.local>
2016-12-18 00:14:19 -0500

    Some more changes

--- SQL/generator.py
+++ SQL/generator.py
@@ -1,10 +1,20 @@
 #Script for generating the SQL scripts of insertion into the movie ids (SQL stored procedure is failing)
 
+
 #Million Loop
-for i in range(1,10):
+total = 10000000
+num_files = 50
+step = total/num_files
+
+for i in range(1,num_files + 1):
 	with open('insert' + str(i) + '.sql', 'w') as f:
-		for index in range(1000000*(i-1), 1000000*i):
-			f.write("INSERT INTO movie_ids (id, stat) values (" + str(index) + ", 'UC');" + '\n')
+		f.write("INSERT INTO movie_ids (id, stat) values\n")
+		srat = False
+		for index in range(step*(i-1), step*i):
+			if(index == step*i -1):				
+				f.write("(" + str(index) + ", 'UC');")
+			else:	
+			 	f.write("(" + str(index) + ", 'UC')," + '\n')
 
 		print('Finished File: ' + str(i))	
 
